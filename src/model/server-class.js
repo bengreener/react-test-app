ea84aec6bf9d60b11a5f856950e0c7ce0d5a46dd
Ben Greener <53060452+bengreener@users.noreply.github.com>
2023-02-13 18:08:50 +0000

    add files omitted from last commit

---
+++ src/model/server-class.js
@@ -0,0 +1,11 @@
+class ServerClass {
+    get text() {
+        return `It's a server client world :-D`;
+    }
+    
+    getText() {
+        return 'Class functional text ;-)';
+    }
+}
+
+export default ServerClass;

7280bd73a22f5afb37ef5f96e5e0143027aefcd9
Ben Greener <53060452+bengreener@users.noreply.github.com>
2023-02-15 10:17:36 +0000

    add test for a mocked function spy on local class mock
    behviour fails. Possibly related to hoisiting of mocks. Exact cause unknown
    files renamed for consistency

--- src/App-local-mock.test.js
+++ src/App-local-mock.test.js
@@ -11,12 +11,15 @@ jest.mock('./model/server', () => {
 })
 
 // mock class
+// const mockGetText = jest.fn().mockImplementation(() => `local mock ServerClass.getText()`); // fails with ReferenceError: Cannot access 'mockGetText' before initialization
+// failure possibly due to hoisting of mocks; this approach works in a demo (jest-sound-player) that doesn't use React/ create-react-app so missing some understanding of behaviour :-/ 
 jest.mock('./model/server-class', () => {
   return jest.fn().mockImplementation(() => {
     return {
       'text': `local mock ServerClass.text`,
       // getText: function() {return `local mock ServerClass.getText()`}
-      getText: jest.fn(() => `local mock ServerClass.getText()`)
+      getText: jest.fn(() => `local mock ServerClass.getText()`)//,
+      // getText: mockGetText // initialisation of mock function fails with ReferenceError: Cannot access 'mockGetText' before initialization
     };
   })
 });
@@ -43,4 +46,10 @@ it('displays ServerClass.getText() method return value', () => {
   render(<App />);
   const pElement = screen.getByText(/local mock ServerClass.getText()/);
   expect(pElement).toBeInTheDocument();
+})
+
+// required configuration fails with ReferenceError: Cannot access 'mockGetText' before initialization
+xit('calls class mockGetText', () => {
+  render(<App />);
+  expect(mockGetText).toBeCalled();
 })
